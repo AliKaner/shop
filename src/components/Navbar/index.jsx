aee7101e66e5f47938fe505d0aa269d328700079
Ali Kaner <53481701+AliKaner@users.noreply.github.com>
2021-10-14 01:19:29 +0300

    update-9

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import {AppBar, Toolbar, IconButton, Badge, MenuItem, mENU, Typography} from "@material-ui/core";
 import {ShoppingCart} from "@material-ui/icons";
-import useStyles from ''
-const Navbar = () => {
+import useStyle from './style'
+export const Navbar = () => {
+    const classes = useStyle();
     return (
         <>
             <AppBar position= "fixed" className={classes.appBar} color="inherit">
                 <Toolbar>
                     <Typography variant="h6" className={classes.title} color="inherit">
-                        <img src={} alt="Shop.js" height="25px" className={classes.image}/>
+                        <img src='https://www.lifepng.com/wp-content/uploads/2020/07/1594327258_Smoke-png-HD.png' alt="Shop.js" height="25px" className={classes.image}/>
                         Shop
                     </Typography>
                     <div className={classes.grow} />
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import {Grid}from "@material-ui/core";
 import {Product} from "./Product";
-
+import useStyles from "./Product/styles";
  const products = [
     {id: 1, name: 'Shoes' , description: 'Running shoes', price: '$5',image: 'https://images.pexels.com/photos/5834/nature-grass-leaf-green.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500'},
     {id: 2,name: 'Mac' , description: 'iMac-pro', price: '$5', image: 'https://images.pexels.com/photos/762687/pexels-photo-762687.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'},
 ];
 
 export const Products = () => {
+
+    const classes = useStyles();
     return(
-    <main>
+    <main className={classes.content}>
+        <div className={classes.toolbar}/>
         <Grid container justify="center" spacing={4}>
             {products.map((product) => (
                 <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
