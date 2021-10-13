742338c2cbb30bba9b21d27bcbef6bc1f71ddf29
Ali Kaner <53481701+AliKaner@users.noreply.github.com>
2021-10-13 16:44:23 +0300

    update-2

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import Grid from "@material-ui/core";
 
+const products = [
+    {id: 1, name: 'Shoes' , description: 'Running shoes'},
+    {id: 2,name: 'Mac' , description: 'iMac-pro'},
+
+];
+
+
 const Products = () => {
     <main>
         <Grid container justify="center" spacing={4}>
-
+            {products.map((product) => (
+                <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                    <Product />
+                </Grid>
+            ))}
         </Grid>
     </main>
 }
