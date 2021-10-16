195702e9db0e47a0f7d4b33f3499067bdb5b91d7
Ali Kaner <53481701+AliKaner@users.noreply.github.com>
2021-10-16 17:10:58 +0300

    update-10

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {Products} from "./components/Products/Products";
+import {commerce} from "./lib/commerce";
 import {Navbar} from "./components/Navbar";
 
 export const App= () => {
+    const [products,setProducts] = useState([]);
+
+    const fetchProducts = async () => {
+        const { data } = await commerce.products.list();
+        setProducts(data);
+    }
+
+    useEffect(()=>{
+        fetchProducts();
+    }, []);
     return(
         <div>
             <Navbar/>
