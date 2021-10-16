7af61c7955162a2a351a7f8d653c3adae1ddaa29
Ali Kaner <53481701+AliKaner@users.noreply.github.com>
2021-10-16 18:00:51 +0300

    update-11

--- src/App.js
+++ src/App.js
@@ -5,19 +5,27 @@ import {Navbar} from "./components/Navbar";
 
 export const App= () => {
     const [products,setProducts] = useState([]);
+    const [cart, setCart] = useState({});
 
     const fetchProducts = async () => {
         const { data } = await commerce.products.list();
         setProducts(data);
     }
+    const fetchCart = async () => {
+        setCart(await commerce.cart.retrieve())
+    }
 
+    const handleAddToCart = async (productId, quanitity) => {
+        const item = await commerce.cart.add(productId,quanitity);
+        setCart(item.cart);
+    }
     useEffect(()=>{
         fetchProducts();
     }, []);
     return(
         <div>
             <Navbar/>
-            <Products/>
+            <Products products={products} onAddToCart={handleAddToCart()}/>
 
         </div>
     );
