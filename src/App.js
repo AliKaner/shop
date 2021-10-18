8beee12990e425d48b4dd64b108971a65e0d35db
Ali Kaner <53481701+AliKaner@users.noreply.github.com>
2021-10-19 01:01:29 +0300

    update-13

--- src/App.js
+++ src/App.js
@@ -5,26 +5,21 @@ import {Navbar} from "./components/Navbar";
 
 export const App= () => {
     const [products,setProducts] = useState([]);
-    const [cart, setCart] = useState({});
 
     const fetchProducts = async () => {
         const { data } = await commerce.products.list();
         setProducts(data);
     }
-    const handleAddToCart = async (productId, quanitity) => {
-        const item = await commerce.cart.add(productId,quanitity);
-        setCart(item.cart);
-    }
-    useEffect(()=>{
+
+    useEffect(() =>{
         fetchProducts();
-        fetchCart();
-    }, []);
-    console.log(cart);
+    },[]);
+
     console.log(<Products/>)
     return(
         <div>
             <Navbar/>
-            <Products products={products} onAddToCart={handleAddToCart()}/>
+            <Products products={products}/>
 
         </div>
     );
--- src/components/Products/Product/index.js
+++ src/components/Products/Product/index.js
@@ -3,27 +3,26 @@ import {Card, CardMedia, CardContent, CardActions, Typography, IconButton} from 
 import { AddShoppingCart} from "@material-ui/icons";
 
 import useStyles from './styles';
-export const Product = ({product, onAddToCart}) => {
+export const Product = ({product}) => {
 
     const classes = useStyles();
     console.log(product);
-    return <div> dans</div>
     return(
         <Card className ={classes.root}>
-            <CardMedia className={classes.media} image={product.image.source} title={product.name} />
+            <CardMedia className={classes.media} image={product.image.url} title={product.name} />
             <CardContent>
                 <div className={classes.cardContent}>
                     <Typography variant="h5" gutterBottom>
                         {product.name}
                     </Typography>
                     <Typography variant="h5" gutterBottom>
-                        {product.price}
+                        {product.price.formatted}
                     </Typography>
                 </div>
-                <Typography variant="body2" color="textSecondary">{product.description}</Typography>
+                <Typography dangerouslySetInnerHTML={{ __html: product.description }} component="p" variant="body2" color="textSecondary"></Typography>
             </CardContent>
             <CardActions disableSpacing className={classes.cardActions}>
-                <IconButton aria-label="Sepete Ekle" onClick={() => onAddToCart(product.id,1)}>
+                <IconButton aria-label="Sepete Ekle" >
                     <AddShoppingCart/>
                 </IconButton>
             </CardActions>
